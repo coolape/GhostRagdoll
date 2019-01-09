84d03ac93135946804698357ee59beefaec97552
chenbin <chbin12@126.com>
2019-01-09 22:11:21 +0800

    add

---
+++ Assets/ragdoll/Scripts/RdJoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RdJoint : MonoBehaviour
+{
+
+    HingeJoint _joint;
+    public HingeJoint joint
+    {
+        get
+        {
+            if(_joint == null)
+            {
+                _joint = GetComponent<HingeJoint>();
+            }
+            return _joint;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spring(float sping, float targetVal, float speed,
+     AnimationCurve curve, float limitMin, float limitMax, object finishCallback)
+    {
+
+    }
+}
