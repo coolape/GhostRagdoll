3518d40ab5908a843a4739b697813632c84cfc03
chenbin <chbin12@126.com>
2019-01-13 22:20:10 +0800

    add project

---
+++ Assets/ghostRagdoll/upgradeRes4Dev/priority/lua/ui/panel/CLLPLogin.lua
@@ -0,0 +1,144 @@
+﻿-- 用户登陆界面
+do
+    ---@type NetProtoUsermgr
+    local NetProtoUsermgr = require("net.NetProtoUsermgrClient")
+    CLLPLogin = {}
+
+    local csSelf = nil
+    local transform = nil
+    local ButtonEntry
+    local ButtonServer
+    local LabelServerName
+    local LabelServerState
+    local loginContent
+    local oldServerIdx
+    local user
+    local server
+    local finishCallback
+
+    -- 初始化，只会调用一次
+    function CLLPLogin.init(csObj)
+        csSelf = csObj
+        transform = csObj.transform
+        --[[
+        上的组件：getChild(transform, "offset", "Progress BarHong"):GetComponent("UISlider")
+        --]]
+        local bottom = getChild(transform, "AnchorBottom/offset")
+        ButtonEntry = getChild(bottom, "ButtonEntry").gameObject
+        ButtonServer = getChild(bottom, "ButtonServer")
+        LabelServerName = getCC(ButtonServer, "LabelName", "UILabel")
+        LabelServerState = getCC(ButtonServer, "LabelState", "UILabel")
+        ButtonServer = ButtonServer.gameObject
+        loginContent = getChild(transform, "loginContent")
+        -- 网络
+        Net.self:setLua()
+    end
+
+    -- 设置数据
+    function CLLPLogin.setData(paras)
+        finishCallback = paras
+    end
+
+    -- 显示，在c#中。show为调用refresh，show和refresh的区别在于，当页面已经显示了的情况，当页面再次出现在最上层时，只会调用refresh
+    function CLLPLogin.show()
+        SetActive(ButtonEntry, false)
+        SetActive(ButtonServer, false)
+        SetActive(loginContent.gameObject, true)
+    end
+
+    -- 刷新
+    function CLLPLogin.refresh()
+    end
+
+    -- 关闭页面
+    function CLLPLogin.hide()
+    end
+
+    function CLLPLogin.setServer(data)
+        server = data
+        LabelServerName.text = server.name
+        local state = bio2number(server.status)
+        LabelServerState.text = (state == 1 and "[00ffff]正常[-]" or (state == 2 and "[00ff00]爆满[-]" or "[fff0000]维护[-]"))
+        SetActive(ButtonServer, true)
+    end
+
+    -- 网络请求的回调；cmd：指命，succ：成功失败，msg：消息；paras：服务器下行数据
+    function CLLPLogin.procNetwork (cmd, succ, msg, paras)
+        if succ == NetSuccess then
+            if cmd == NetProtoUsermgr.cmds.loginAccountChannel
+                    or cmd == NetProtoUsermgr.cmds.loginAccount
+                    or cmd == NetProtoUsermgr.cmds.registAccount then
+                hideHotWheel()
+                user = paras.userInfor
+                NetProtoUsermgr.__sessionID = bio2Int(paras.userInfor.idx)
+                -- 初始化时间
+                DateEx.init(bio2number(paras.systime))
+                -- 取得服务器
+                oldServerIdx = bio2number(paras.serverid)
+                if oldServerIdx > 0 then
+                    showHotWheel()
+                    CLLNet.httpPostUsermgr(NetProtoUsermgr.send.getServerInfor(oldServerIdx))
+                else
+                    printw("get the server id == 0")
+                end
+            elseif cmd == NetProtoUsermgr.cmds.getServerInfor then
+                CLLPLogin.setServer(paras.server)
+                SetActive(loginContent.gameObject, false)
+                SetActive(ButtonEntry, true)
+                hideHotWheel()
+            end
+        else
+            hideHotWheel()
+            CLAlert.add(msg)
+        end
+    end
+
+    -- 处理ui上的事件，例如点击等
+    function CLLPLogin.uiEventDelegate( go )
+        local goName = go.name
+
+        if goName == "ButtonVisitor" then
+            local deviceInfor = {}
+            table.insert(deviceInfor, SystemInfo.deviceName)
+            table.insert(deviceInfor, SystemInfo.deviceModel)
+            table.insert(deviceInfor, SystemInfo.deviceType:ToString())
+            table.insert(deviceInfor, SystemInfo.operatingSystem)
+            table.insert(deviceInfor, SystemInfo.maxTextureSize)
+            showHotWheel()
+            CLLNet.httpPostUsermgr(NetProtoUsermgr.send.loginAccountChannel(Utl.uuid,
+                    CLCfgBase.self.appUniqueID,
+                    getChlCode(),
+                    Utl.uuid,
+                    table.concat(deviceInfor, ",")))
+        elseif goName == "ButtonServer" then
+            --CLPanelManager.getPanelAsy("PanelServers", onLoadedPanelTT, { CLLPLogin.onSelectServer, selectedServer })
+        elseif goName == "ButtonEntry" then
+            SetActive(ButtonEntry, false)
+            local state = bio2number(server.status)
+            if state ~= "1" then
+                -- 服务器停服了
+                CLAlert.add(LGet("UIMsg005"), Color.red, 1)
+                SetActive(ButtonEntry, true)
+                return
+            end
+            hideTopPanel(csSelf)
+            if oldServerIdx ~= bio2number(server.idx) then
+                -- 保存所选的服务器
+                CLLNet.httpPostUsermgr(NetProtoUsermgr.send.setEnterServer(bio2number(server.idx), bio2number(user.idx), CLCfgBase.self.appUniqueID))
+            end
+            Utl.doCallback(finishCallback, user, server)
+        elseif goName == "ButtonXieyi" then
+            --CLPanelManager.getPanelAsy("PanelRobotTest", onLoadedPanelTT, { MapEx.getString(user, "idx"), selectedServer })
+        elseif goName == "ButtonSetting" then
+            --getPanelAsy("PanelSetting", onLoadedPanelTT)
+        end
+    end
+
+    -- 当按了返回键时，关闭自己（返值为true时关闭）
+    function CLLPLogin.hideSelfOnKeyBack( )
+        return true
+    end
+
+    --------------------------------------------
+    return CLLPLogin
+end
---
+++ Assets/ghostRagdoll/upgradeRes4Dev/priority/lua/ui/panel/CLLPStart.lua
@@ -0,0 +1,206 @@
+﻿--开始游戏
+do
+    local csSelf = nil
+    local transform = nil
+    local gameObject = nil
+    local upgradeUrl
+    local user
+    local uidx
+    local selectedServer
+    local panelIndex = 0
+
+    -- 放在后面加载的页面
+    local lateLoadPanels = {
+        "PanelSceneManager", -- 切换场景页面
+    }
+
+    local isLogined = false
+    CLLPStart = {}
+
+    function CLLPStart.init(go)
+        csSelf = go
+        transform = csSelf.transform
+        gameObject = csSelf.gameObject
+        -- 加载一些必要的lua
+        CLLPStart.setLuasAtBegainning()
+    end
+
+    -- 加载一些必要的lua
+    function CLLPStart.setLuasAtBegainning()
+        -- 取得数据配置
+        require("cfg.DBCfg")
+        -- 网络
+        Net.self:setLua()
+        -- 资源释放时间
+        if CLAssetsManager.self then
+            CLAssetsManager.self.timeOutSec4Realse = 10
+        end
+
+        if ReporterMessageReceiver.self and ReporterMessageReceiver.self.gameObject then
+            if KKWhiteList.isWhiteName() then
+                ReporterMessageReceiver.self.gameObject:SetActive(true)
+            else
+                ReporterMessageReceiver.self.gameObject:SetActive(false)
+            end
+        end
+        CLPanelManager.self.mainPanelName = "PanelMain"
+        -- 添加屏蔽字
+        MyMain.self:invoke4Lua(CLLPStart.addShieldWords, 1)
+
+        --TODO:other lua scripts
+    end
+
+    function CLLPStart.setData(pars)
+        user = pars[1]
+        selectedServer = pars[2]
+    end
+
+    function CLLPStart.show()
+        --CLLPStart.createPanel()
+    end
+
+    -- 刷新页面
+    function CLLPStart.refresh()
+        if not isLogined then
+            CLLPStart.createPanel()
+        end
+    end
+
+    -- 关闭页面
+    function CLLPStart.hide()
+        csSelf:cancelInvoke4Lua()
+    end
+
+    -- 创建ui
+    function CLLPStart.createPanel()
+        panelIndex = 0
+        local count = #(lateLoadPanels)
+        if (count > 0) then
+            for i = 1, count do
+                local name = lateLoadPanels[i]
+                CLPanelManager.getPanelAsy(name, CLLPStart.onLoadPanelAfter)
+            end
+        else
+            CLLPStart.connectServer()
+        end
+    end
+
+    function CLLPStart.onLoadPanelAfter(p)
+        p:init()
+        panelIndex = panelIndex + 1
+        local count = #(lateLoadPanels)
+        if (panelIndex >= count) then
+            --已经加载完
+            CLLPStart.connectServer()
+        end
+    end
+
+    -- 添加屏蔽字
+    function CLLPStart.addShieldWords()
+        local onGetShieldWords = function(path, content, originals)
+            if (content ~= nil) then
+                BlockWordsTrie.getInstanse():init(content)
+            end
+        end
+        local path = joinStr(CLPathCfg.self.basePath, "/", CLPathCfg.upgradeRes, "/priority/txt/shieldWords")
+        CLVerManager.self:getNewestRes(path, CLAssetType.text, onGetShieldWords, nil)
+    end
+
+    -- 连接服务器相关处理
+    function CLLPStart.connectServer()
+        showHotWheel()
+        Net.self:connectGame(selectedServer.host, selectedServer.port)
+    end
+
+    -- 处理网络接口
+    function CLLPStart.procNetwork(cmd, succ, msg, pars)
+        if (succ == NetSuccess) then
+            -- 接口处理成功
+            if (cmd == "connectCallback") then
+                --[[
+                if (pars == Net.self.gateTcp) then
+                end
+                ]]
+                --hideHotWheel()
+                --showHotWheel()
+
+                local uid = bio2Int(user.idx)
+                if CLCfgBase.self.isEditMode then
+                    if not isNilOrEmpty(MyCfg.self.default_UID) then
+                        uid = MyCfg.self.default_UID
+                    elseif (not isNilOrEmpty(__UUID__)) then
+                        uid = __UUID__
+                    end
+                else
+                    if not isNilOrEmpty(__UUID__) then
+                        uid = __UUID__
+                    end
+                end
+
+                --PorotocolService.callNet.__sessionid = uid
+                --Net.self:send(PorotocolService.callNet.login(uid))
+            elseif cmd == "login" then
+                --hideHotWheel()
+                --if msg == "-1" then
+                --    CLPanelManager.getPanelAsy("PanelSelectPlayer", onLoadedPanelTT, bio2Int())
+                --else
+                --    CLLPStart.doEnterGame()
+                --end
+                --
+                ---- IAP 登陆成功后再初化iap
+                --local rt, err = pcall(KKChlIAP.init)
+                --if not rt then
+                --    printe(err)
+                --end
+            end
+        else
+            -- 接口返回不成功
+            if (cmd == "outofNetConnect") then
+                hideHotWheel()
+                CLUIUtl.showConfirm(
+                        LGet("UIOutofConnect"),
+                        CLLPStart.connectServer,
+                        function()
+                            local p = CLPanelManager.getPanel("PanelSplash")
+                            if (p ~= nil) then
+                                CLPanelManager.showPanel(p)
+                            end
+                            hideTopPanel()
+                        end
+                )
+            else
+                CLAlert.add(msg, Color.red, 1)
+            end
+        end
+    end
+
+    -- 点击返回键关闭自己（页面）
+    function CLLPStart.hideSelfOnKeyBack()
+        return false
+    end
+
+    function CLLPStart.doEnterGame()
+        local tasks = KKDBUser.getTasks()
+        if tasks == nil then
+            return
+        end
+
+        CLLPStart._EnterGame(KKDBUser.isNewPlayer())
+    end
+
+    function CLLPStart._EnterGame(isNewPlayer)
+        CLPanelManager.getPanelAsy("PanelSceneManager",
+                function(p)
+                    p:setData({ mode = GameMode.city })
+                    CLPanelManager.showTopPanel(p)
+
+                    local p2 = CLPanelManager.getPanel("PanelSplash")
+                    if (p2 ~= nil) then
+                        CLPanelManager.hidePanel(p2)
+                    end
+                end)
+    end
+
+    ----------------------------------------------
+    return CLLPStart
+end
